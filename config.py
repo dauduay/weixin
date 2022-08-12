92dc44ae3f334d006aac018c98d778c19a472c61
dauduay <111147121+dauduay@users.noreply.github.com>
2022-08-13 03:33:33 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
 # 公众号配置
 # 公众号appId
-app_id = ""
+app_id = "wxbde606cf054ac667"
 # 公众号appSecret
-app_secret = ""
+app_secret = "db863a39d53b9431b6254cc68a950a2b"
 # 模板消息id
-template_id = ""
+template_id = "4osl9jJKd_Um1tak1eeYyPScSABcujYteP238V9V87k"
 # 接收公众号消息的微信号
-user = ""
+user = "obBHL6dvkKwZjCs4U1H4Sr3Em0iA"
 
 # 信息配置
 # 所在省份
-province = "广东"
+province = "北京"
 # 所在城市
-city = "广州"
+city = "北京"
 # 生日，如果月份或者日期小于10，直接用对应的数字即可，例如1997-1-1
-birthday = "1997-1-1"
+birthday = "2006-2-13"
 # 在一起的日子，格式同上
-love_date = "2022-1-1"
+love_date = "2022-8-14"
 
